d9a7f955fda5dec1ddbbe9d884703173eeb95a1f
Chumpol Mokarat <chumpol_mo@rmutto.ac.th>
2024-06-27 16:34:29 +0700

    Commit first react demo.

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function MyBlog() {
+    return (
+      <>
+        <h1>Hello, My Blog!</h1>
+        <a href="/">Back</a>
+      </>
+    );
+  }
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function MyDashboard() {
+    return <h1>Hello, My Dashboard!</h1>
+  }
